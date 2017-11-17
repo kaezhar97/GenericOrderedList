01fe07fed09b15e579352271c9fac2db37430f1e
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-11-17 15:52:45 -0500

    Added a ton of comments. The skeleton of the GenericOrderedList class and a new test class

--- src/GenericOrderedList.java
+++ src/GenericOrderedList.java
@@ -1,4 +1,14 @@
+/*
+File: GenericOrderedList.java
+Purpose:
+    Creates an ordered list with the specified objects in ascending order initially. New additions are included in the right order.
+This class implements a singly linked list.
+*/
+
+
+
 /**
+ * 
  * A class that simulates an ordered list of any object. New objects added to the list are inserted in order (ascending or descending)
  * 
  * @author Octavio Avila-Cardet
@@ -18,9 +28,69 @@ public class GenericOrderedList <E>
     }
     
     /**
+     * A method that inserts a new node containing its parameter in its proper place in the list
+     * 
+     * @param x the information contained inside the node to be inserted
+     */
+    public void insert(E x)
+    {
+        
+    }
+    
+    /**
+     * Searches the list for a node containing its parameter
+     * 
+     * @param x the information contained inside the node to be deleted
+     * @return true is returned if node is found; else, false is returned
+     */
+    public boolean delete(E x)
+    {
+        boolean found = false;
+        
+        return found;
+    }
+    
+    /**
+     * Checks whether or not the list is empty
+     * 
+     * @return true if list is empty; false otherwise
+     */
+    public boolean isEmpty()
+    {
+        boolean listIsEmpty = false;
+        
+        return listIsEmpty;
+    }
+    
+    /**
+     * Deletes all nodes in the list
+     */
+    public void clear()
+    {
+        
+    }
+    
+    /**
+     * Reverses the order of the nodes in the list
+     */
+    public void reverse()
+    {
+        
+    }
+    
+    /**
+     * Displays the generic ordered list in textually
+     * 
+     * @return the final string with the information of each node on the list in order
+     */
+    public String toString()
+    {
+        String finalString="";
+        return finalString;
+    }
+    
+    /**
      * A class to simulate a node on a list
-     * 
-     * 
      */
     class Node
     {
---
+++ src/ListTest.java
@@ -0,0 +1,14 @@
+/*
+File: ListTest.java
+ * Purpose:
+    Tests the GenericOrderedList.java
+ */
+
+/**
+ *
+ * @author Octavio Avila-Cardet
+ */
+public class ListTest
+{
+    
+}
